16ac097113ed443615eee3886608f9afdada4cf5
beatrizjj <beatrizjordao15@gmail.com>
2024-03-25 21:48:13 -0300

    FOTO

--- Tkinter_estudo.py/Labels.py
+++ Tkinter_estudo.py/Labels.py
@@ -9,13 +9,25 @@ wd = Tk()
 
 #       labels
 
+'''
+TODA FOTO QUE EU FOR USAR TEM QUE ESTAR NO MESMO DIRETÓRIO DO ARQUIVO PYTHON
+OU EU TENHO QUE PASSAR O CAMINHO DA FOTO.
+'''
+
+foto = PhotoImage(file='computador.png')        #Foto que eu escolhi. 
+
+
 caixa = Label(wd,                  # caixa para texto dentro da janela window
-              text='Hello, World',            # texto dentro da label
+              text='Lets code?',            # texto dentro da label
               font=('Arial',40,'bold'),         # fonte, tamanho e estilo
-              fg='#4287f5',                  # cor do texto
-              bg='#2c403f',               # cor da parte de tras do texto
+              fg='#511799',                  # cor do texto 
+              bg='#140469',               # cor da parte de tras do texto
               relief= RAISED,          # estilo da borda da caixa de texto 
-              bd=20)           #tamanho da borda
+              bd=30,           # tamanho da borda
+              padx=20,      # distância do texto para a borda horizontalmente
+              pady=20,          # ''    ''  ''    '' ''  ''   verticalmente
+              image=foto,       # adiciona a imagem à caixa de texto 
+              compound='bottom')      # Diz aonde a fot vai ficar em relação ao texto    
 
 
 # caixa.place(x=600,y=350)       # Não preciso do caixa.pack() quando uso o place
