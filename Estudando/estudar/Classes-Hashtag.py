e1beb15b6aca8a0882f4ec26c46c2719885766ac
bibiperigosa <beatrizjordao15@gmail.com>
2024-04-10 11:39:18 -0300

    aads

--- Estudando/estudar/Classes-Hashtag.py
+++ Estudando/estudar/Classes-Hashtag.py
@@ -1,1 +1,11 @@
 # OPP
+
+class Pessoa:
+    def __init__(self, nome, cpf, sexo):
+        self.nome = nome
+        self.sexo = sexo
+        self.cpf = cpf
+
+if __name__=='__main__':
+    pessoa1 = Pessoa('bia', '16646401433', 'F')
+    print(pessoa1.nome) 
