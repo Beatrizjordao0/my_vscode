54accf8199405727b832b60a3206233e7d160eed
bibiperigosa <beatrizjordao15@gmail.com>
2024-04-10 12:07:27 -0300

    hjh

--- Estudando/estudar/Classes-Hashtag.py
+++ Estudando/estudar/Classes-Hashtag.py
@@ -1,5 +1,5 @@
 # OPP
-
+km = 1
 class Pessoa:
     def __init__(self, nome, cpf, sexo):
         self.nome = nome
@@ -8,4 +8,37 @@ class Pessoa:
 
 if __name__=='__main__':
     pessoa1 = Pessoa('bia', '16646401433', 'F')
-    print(pessoa1.nome) 
+    pessoa2 = Pessoa('Carlos', '44448881898', 'M')
+
+
+class Carro:
+    # Atributos
+    def __init__(self, modelo, placa, ano):
+        self.modelo = modelo
+        self.placa = placa
+        self.ano = ano
+
+    # Métodos
+    def andar():
+        km_andados = int(input('Quantos km você quer andar com o carro?'))
+        km = km * km_andados
+        if km == 0:
+            print('Você não saiu do lugar.')
+        elif km < 0:
+            print(f'Você andou para trás.')
+        else:
+            print(f'Você andou {km} kilômetros')
+
+
+    
+
+if __name__ == '__main__':
+    palio = Carro('palio', 'BNKL-5843', '2015')
+
+    palio.andar
+
+        
+    
+    
+
+    
