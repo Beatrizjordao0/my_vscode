1acba3f6ca93e680a38fc5d2bdd0ebf1227f9917
bibiperigosa <beatrizjordao15@gmail.com>
2024-04-19 10:09:24 -0300

    getting data

---
+++ Estudando/estudar/bancos-de-dados/Getting_data.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+# initialize the database
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='2233287774449999',
+    database='testdb'
+)
+
+mycursor = mydb.cursor()
+# execute command to get the data
+mycursor.execute('SELECT name FROM students')
+# store the data
+myresult = mycursor.fetchall()     # fetch means 'buscar' so fetch all means 'buscar tudo'
+# show the data         # we also can fetchone, which will return for us only the first column of our table
+for row in myresult:
+    print(row)
